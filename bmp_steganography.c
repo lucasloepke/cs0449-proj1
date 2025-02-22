90d8e342c5dcd84f1a41110a59ef6946cf16491b
Lucas Loepke <wavyknight72@gmail.com>
2025-02-22 16:41:17 -0500

    info done

--- bmp_steganography.c
+++ bmp_steganography.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #pragma pack(1)
 typedef struct {
@@ -14,12 +15,12 @@ typedef struct {
     int width;                      // Width of the image in pixels
     int height;                     // Height of the image in pixels
     unsigned short planes;          // Number of color planes (don’t worry)
-    unsigned short bitCount;        // 	Number of bits per pixel
+    unsigned short bitCount;        // Number of bits per pixel
     unsigned int compression;       // Compression scheme used
     unsigned int imageSize;         // Image size in bytes
     int xPixelsPerMeter;            // Horizontal resolution
-    int yPixelsPerMeter;            // 	Vertical resolution
-    unsigned int colorsUsed;        // 	Number of colors in the palette
+    int yPixelsPerMeter;            // Vertical resolution
+    unsigned int colorsUsed;        // Number of colors in the palette
     unsigned int colorsImportant;   // Number of important colors
 } DIBHeader;
 
@@ -30,16 +31,65 @@ typedef struct {
 } Pixel;
 #pragma pack(4)
 
-int main(int argc, char *argv[]) {
-    printf("Number of arguments: %d\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
-    }
-    return 0;
-}
-
 void info(const char *filename) {
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("ERROR: File not found.\n");
+        return;
+    }
 
+    BMPHeader bmpHeader;
+    DIBHeader dibHeader;
+
+    // Read BMP header
+    if (fread(&bmpHeader, sizeof(BMPHeader), 1, file) != 1) {
+        printf("ERROR: The format is not supported.\n");
+        fclose(file);
+        return;
+    }
+
+    // Check for BMP file
+    if (bmpHeader.type != 0x4D42) { // BM in ASCII, reversed for little endian
+        printf("ERROR: The format is not supported.\n");
+        fclose(file);
+        return;
+    }
+
+    // Read DIB header
+    if (fread(&dibHeader, sizeof(DIBHeader), 1, file) != 1) {
+        printf("ERROR: The format is not supported.\n");
+        fclose(file);
+        return;
+    }
+
+    // Check DIB header size and RGB bits per pixel
+    if (dibHeader.headerSize != 40 || dibHeader.bitCount != 24) {
+        printf("ERROR: The format is not supported.\n");
+        fclose(file);
+        return;
+    }
+
+    printf("=== BMP Header ===\n");
+    printf("Type: BM\n");
+    printf("Size: %u\n", bmpHeader.size);
+    printf("Reserved 1: %hu\n", bmpHeader.reserved1);
+    printf("Reserved 2: %hu\n", bmpHeader.reserved2);
+    printf("Image offset: %u\n\n", bmpHeader.offset);
+
+    printf("=== DIB Header ===\n");
+    printf("Size: %u\n", dibHeader.headerSize);
+    printf("Width: %d\n", dibHeader.width);
+    printf("Height: %d\n", dibHeader.height);
+    printf("# color planes: %hu\n", dibHeader.planes);
+    printf("# bits per pixel: %hu\n", dibHeader.bitCount);
+    printf("Compression scheme: %u\n", dibHeader.compression);
+    printf("Image size: %u\n", dibHeader.imageSize);
+    printf("Horizontal resolution: %d\n", dibHeader.xPixelsPerMeter);
+    printf("Vertical resolution: %d\n", dibHeader.yPixelsPerMeter);
+    printf("# colors in palette: %u\n", dibHeader.colorsUsed);
+    printf("# important colors: %u\n", dibHeader.colorsImportant);
+
+    fclose(file);
 }
 
 void reveal(const char *filename) {
@@ -48,4 +98,34 @@ void reveal(const char *filename) {
 
 void hide(const char *filename1, const char *filename2) {
 
+}
+
+int main(int argc, char *argv[]) {
+    // printf("Number of arguments: %d\n", argc);
+    // for (int i = 0; i < argc; i++) {
+    //     printf("argc: %d\n", argc);
+    //     printf("Argument %d: %s\n", i, argv[i]);
+    // }
+
+    if (argc < 3) {
+        printf("ERROR: Missing Arguments.\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1], "--info") == 0) {
+        info(argv[2]);
+    } else if (strcmp(argv[1], "--reveal") == 0) {
+        reveal(argv[2]);
+    } else if (strcmp(argv[1], "--hide") == 0) {
+        if (argc < 4) {
+            printf("ERROR: Missing Arguments.\n");
+            return 1;
+        }
+        hide(argv[2], argv[3]);
+    } else {
+        printf("ERROR: Invalid Arguments.\n");
+        return 1;
+    }
+
+    return 0;
 }
